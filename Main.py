4fa47291c4ef5a53ce4e7bed9eaa96aa85584b91
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-08-19 17:09:19 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -46,54 +46,97 @@ with st.sidebar:
 # Function to create heatmap
 def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
     heatmap = alt.Chart(input_df).mark_rect().encode(
-        y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Ano", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-        x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-        color=alt.Color(f'max({input_color}):Q',
-                        legend=None,
-                        scale=alt.Scale(scheme=input_color_theme)),
-        stroke=alt.value('black'),
-        strokeWidth=alt.value(0.25),
-    ).properties(
-        width='container'  # Adjust width based on the container to make it responsive
-    ).configure_axis(
+            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Ano", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+            color=alt.Color(f'max({input_color}):Q',
+                             legend=None,
+                             scale=alt.Scale(scheme=input_color_theme)),
+            stroke=alt.value('black'),
+            strokeWidth=alt.value(0.25),
+        ).properties(width=900
+        ).configure_axis(
         labelFontSize=12,
         titleFontSize=12
-    )
+        ) 
+    # height=300
     return heatmap
 
-# Function to create donut chart
+# Choropleth map
+#def make_choropleth(input_df, input_id, input_column, input_color_theme):
+    #choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
+                               #color_continuous_scale=input_color_theme,
+                               #range_color=(0, max(df_selected_year.population)),
+                               #scope="usa",
+                               #labels={'population':'Population'}
+                              #)
+    #choropleth.update_layout(
+        #template='plotly_dark',
+        #plot_bgcolor='rgba(0, 0, 0, 0)',
+        #paper_bgcolor='rgba(0, 0, 0, 0)',
+        #margin=dict(l=0, r=0, t=0, b=0),
+        #height=350
+    #)
+    #return choropleth
+
+
+# Donut chart
 def make_donut(input_response, input_text, input_color):
-    # Adjust colors based on the selected color theme
-    chart_color = {
-        'blue': ['#29b5e8', '#155F7A'],
-        'green': ['#27AE60', '#12783D'],
-        'orange': ['#F39C12', '#875A12'],
-        'red': ['#E74C3C', '#781F16']
-    }[input_color]
+  if input_color == 'blue':
+      chart_color = ['#29b5e8', '#155F7A']
+  if input_color == 'green':
+      chart_color = ['#27AE60', '#12783D']
+  if input_color == 'orange':
+      chart_color = ['#F39C12', '#875A12']
+  if input_color == 'red':
+      chart_color = ['#E74C3C', '#781F16']
+    
+  source = pd.DataFrame({
+      "Topic": ['', input_text],
+      "% value": [100-input_response, input_response]
+  })
+  source_bg = pd.DataFrame({
+      "Topic": ['', input_text],
+      "% value": [100, 0]
+  })
+    
+  plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode(
+      theta="% value",
+      color= alt.Color("Topic:N",
+                      scale=alt.Scale(
+                          #domain=['A', 'B'],
+                          domain=[input_text, ''],
+                          # range=['#29b5e8', '#155F7A']),  # 31333F
+                          range=chart_color),
+                      legend=None),
+  ).properties(width=130, height=130)
+    
+  text = plot.mark_text(align='center', color="#29b5e8", font="Lato", fontSize=32, fontWeight=700, fontStyle="italic").encode(text=alt.value(f'{input_response} %'))
+  plot_bg = alt.Chart(source_bg).mark_arc(innerRadius=45, cornerRadius=20).encode(
+      theta="% value",
+      color= alt.Color("Topic:N",
+                      scale=alt.Scale(
+                          # domain=['A', 'B'],
+                          domain=[input_text, ''],
+                          range=chart_color),  # 31333F
+                      legend=None),
+  ).properties(width=130, height=130)
+  return plot_bg + plot + text
 
-    source = pd.DataFrame({
-        "Topic": ['', input_text],
-        "% value": [100-input_response, input_response]
-    })
+# Convert population to text 
+def format_number(num):
+    if num > 1000000:
+        if not num % 1000000:
+            return f'{num // 1000000} M'
+        return f'{round(num / 1000000, 1)} M'
+    return f'{num // 1000} K'
 
-    plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode(
-        theta="% value",
-        color=alt.Color("Topic:N",
-                        scale=alt.Scale(
-                            domain=[input_text, ''],
-                            range=chart_color),
-                        legend=None),
-    ).properties(
-        width=130, height=130  # The chart size should be relatively small for mobile compatibility
-    )
+# Calculation year-over-year population migrations
+def calculate_population_difference(input_df, input_year):
+  selected_year_data = input_df[input_df['year'] == input_year].reset_index()
+  previous_year_data = input_df[input_df['year'] == input_year - 1].reset_index()
+  selected_year_data['population_difference'] = selected_year_data.population.sub(previous_year_data.population, fill_value=0)
+  return pd.concat([selected_year_data.states, selected_year_data.id, selected_year_data.population, selected_year_data.population_difference], axis=1).sort_values(by="population_difference", ascending=False)
 
-    text = plot.mark_text(align='center', color=chart_color[0], font="Lato", fontSize=32, fontWeight=700, fontStyle="italic").encode(
-        text=alt.value(f'{input_response} %')
-    )
-
-    return plot + text
-
-# Function to simulate population step
 def simulate_population_step(population, birth_rate, death_rate):
     births = np.random.poisson(birth_rate)
     deaths = np.random.poisson(death_rate)
@@ -102,23 +145,24 @@ def simulate_population_step(population, birth_rate, death_rate):
     
     return new_population, births, deaths
 
-# Function to compute statistics
+# Função para calcular as estatísticas
 def compute_statistics(data):
     mean = np.mean(data)
+    #mode = stats.mode(data)[0][0]
     std_dev = np.std(data)
     variance = np.var(data)
     
     return mean, std_dev, variance
 
-# Function to perform regression
+# Função para realizar a regressão linear
 def perform_regression(time, population):
     X = sm.add_constant(time)
     model = sm.OLS(population, X).fit()
     return model
 
-#######################
-# Main Panel
 
+#######################
+# Dashboard Main Panel
 col = st.columns((1.5, 4.5, 2), gap='medium')
 
 with col[0]:
@@ -138,28 +182,42 @@ with col[0]:
 
     if selected_year > 2010:
         last_state_name = df_population_difference_sorted.states.iloc[-1]
-        last_state_population = format_number(df_population_difference_sorted.population.iloc[-1])
-        last_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[-1])
+        last_state_population = format_number(df_population_difference_sorted.population.iloc[-1])   
+        last_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[-1])   
     else:
         last_state_name = '-'
         last_state_population = '-'
         last_state_delta = ''
     st.metric(label=last_state_name, value=last_state_population, delta=last_state_delta)
 
+    
     st.markdown('#### States Migration')
 
     if selected_year >= 2010:
+        # Filter states with population difference > 50000
+        # df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference_absolute > 50000]
         df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference > 50000]
         df_less_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference < -50000]
+        
+        
 
+        
+        # % of States with population difference > 50000
+        #states_migration_greater= kpi2.metric(label="Nascimentos no último segundo", value=int(births))
+        #states_migration_less = kpi3.metric(label= "Mortes no último segundo", value=int(deaths))
         states_migration_greater = round((len(df_greater_50000)/df_population_difference_sorted.states.nunique())*100)
         states_migration_less = round((len(df_less_50000)/df_population_difference_sorted.states.nunique())*100)
+        #donut_chart_greater = make_donut(births_data, 'Births', 'green')
         donut_chart_greater = make_donut(states_migration_greater, 'Births', 'green')
+        #donut_chart_less = make_donut(deaths_data, 'Deaths', 'red')
         donut_chart_less = make_donut(states_migration_less, 'Deaths', 'red')
     else:
         states_migration_greater = 0
         states_migration_less = 0
+        #donut_chart_greater = make_donut(births_data, 'Births', 'green')
         donut_chart_greater = make_donut(states_migration_greater, 'Births', 'green')
+        #donut_chart_less = make_donut(deaths_data, 'Deaths', 'red')
+        #donut_chart_greater = make_donut(states_migration_greater, 'Inbound Migration', 'green')
         donut_chart_less = make_donut(states_migration_less, 'Deaths', 'red')
 
     migrations_col = st.columns((0.2, 1, 0.2))
